12961a1683f3ea9fb5c7d39771bcc94a922022bc
Jiaoquan Chen <j_chen2010@hotmail.com>
2016-07-05 03:20:40 +0000

    added Guides.js

--- react-calendar.js
+++ react-calendar.js
@@ -4,3 +4,4 @@ export { default as Week } from './src/Week';
 export { default as Day } from './src/Day';
 export { default as util } from './src/util';
 export { default as dateUtils } from './src/dateUtils';
+export { default as Guides } from './src/Guides.js';
