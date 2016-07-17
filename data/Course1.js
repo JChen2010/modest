73dd37c6015435fe1cfc1d10b41a59905562cc7f
levonamatuni <levonamatuni@gmail.com>
2016-07-16 23:21:35 -0400

    - minor errors

--- data/Course1.js
+++ data/Course1.js
@@ -1,10 +1,17 @@
-var vocab1 = ["Case Studies", [
+var vocab1 = [["Case Studies 1", [
 		["Uber for failure", 
 			"Read case study at: http://mashable.com/2016/04/20/uber-for-failure/#Ugzc72E5agq3.\nSummarize case study.\nTips: Key people, companies, numbers.\nStudy Question:\nWhy did Shuttle fail?"],
 		["Biocon India Group", 
 			"Watch video at: http://bioconindiagroup.com/intro.\nStudy Questions \n1. What  are  the  advantages  and  disadvantages  of starting  and  operating  a  pharmaceutical  firm  in India? \n2. Is the Indian CRO market attractive? \n3. What is the best way for Biocon India Group to expand?<b>Case Write-up Question</b>\nWhat is the best way for Biocon India Group to expand, and what factors should it consider?"]
 	]
-];
+],
+["Case Studies 2", [
+		["Uber for failure", 
+			"Read case study at: http://mashable.com/2016/04/20/uber-for-failure/#Ugzc72E5agq3.\nSummarize case study.\nTips: Key people, companies, numbers.\nStudy Question:\nWhy did Shuttle fail?"],
+		["Biocon India Group", 
+			"Watch video at: http://bioconindiagroup.com/intro.\nStudy Questions \n1. What  are  the  advantages  and  disadvantages  of starting  and  operating  a  pharmaceutical  firm  in India? \n2. Is the Indian CRO market attractive? \n3. What is the best way for Biocon India Group to expand?<b>Case Write-up Question</b>\nWhat is the best way for Biocon India Group to expand, and what factors should it consider?"]
+	]
+]];
 
 module.exports = vocab1;
 
