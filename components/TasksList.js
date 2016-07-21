dfcca9ccbcbebd333881c1b8493462ff9bb9e732
levonamatuni <levonamatuni@gmail.com>
2016-07-21 01:20:34 -0400

    - Quiz button

--- components/TasksList.js
+++ components/TasksList.js
@@ -6,32 +6,56 @@ var TasksList = React.createClass({
   propTypes: {
     lesson: PropTypes.array.isRequired	
   },	
- 
+  
+  getInitialState: function () {
+    return {
+      Quiz: false
+    }
+  },
+
+  handleQuizStart: function (e) {
+    var index = e.target.getAttribute('data-index'); 
+    this.setState({
+      Quiz: true
+    });
+  },
+
   render: function () {
+    if (this.state.Quiz) {
+      return (
+        <div>
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h2 className="panel-title">Quiz Started!</h2>           
+          </div>
+          <div className="panel-body">
+            Questions
+          </div>
+        </div>
+        </div>
+      )
+    } else {
     return (
     	 <div className="tasks-list">
           {
-            this.props.lesson.map(function(task){     
+            this.props.lesson.map(function(task, i){     
               return (   
                 <div className="panel panel-default">
                     <div className="panel-heading">
                         <h2 className="panel-title">{task[0]}</h2>
-                        <h6>Time: {task[2]} min</h6>
+                        <h6>Time: {(task[0] != "Quiz") ? task[2] : task[2].time} min</h6>
                     </div>
                     <div className="panel-body">
                         <p dangerouslySetInnerHTML={{__html: task[1].replace(/\n/g, '<br>')}} />
+                        {(task[0] != "Quiz") ? "" : <button type="button" className="btn btn-warning" data-index={i} onClick={this.handleQuizStart}>Start</button>}
                     </div>
                 </div>
               )
-            })
-          }
-
-          
-            
+            }.bind(this))
+          } 
         </div>
-
-  
 	    )
+  }
   }
 });
 
