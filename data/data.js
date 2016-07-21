69d8875f559689ca3ef48cc7acec94db77831257
Jeffrey Chen <Jeffrey Chen>
2016-07-21 05:21:24 -0400

    Test data file

---
+++ data/data.js
@@ -0,0 +1,138 @@
+var reading = 
+[
+	[
+		["Summarization - HSBC",
+			[
+				["Read Article",
+					"Read article at http://www.nytimes.com/2016/07/21/business/dealbook/hsbc-foreign-exchange-investigation-currency.html on HSBC's foreign exchange investigation currency.",
+					20
+				],
+
+				["Summarize", "Write a 500 word summary of the article and bring it in to class tomorrow.", 40]
+			]
+		],
+
+		["Summarization - Wonderland",
+			[
+				["Read Article",
+				"Read article at http://blog.wonderland.com/new-rides-this-season", 20
+				],
+
+				["Summarize", "Write a 500 word summary of the article and bring it in to class tomorrow.", 40
+				]
+			]
+		]
+	],
+	
+	[
+		["Article Jigsaw - Thumb Sucking",
+			[
+				["Read Article", 
+					"Read article at http://www.health.harvard.edu/blog/upside-thumb-sucking-nail-biting-2016071910032)", 20
+				],
+
+				["Unscramble", 
+				"The passage in the link below is the same as the article above with the paragraphs out of order. Put the paragraphs back in order. http://www.health.harvard.edu/blog/upside-thumb-sucking-nail-biting-2016071910032/reorder", 20]
+			]
+		],
+
+		["Article Jigsaw - Climate",
+			[
+				["Read Article", 
+					"Read article at http://phys.org/news/2016-07-north-american-forests-climate.html)", 15
+				],
+
+				["Unscramble", 
+				"The passage in the link below is the same as the article above with the paragraphs out of order. Put the paragraphs back in order. http://phys.org/news/2016-07-north-american-forests-climate.htmlreorder", 20]
+			]
+		],
+
+		["Article Jigsaw - Health Insurance",
+			[
+				["Read Article",
+					"Read article at http://www.nytimes.com/2016/07/21/business/failure-of-2-health-insurer-mergers-is-unlikely-to-stop-the-efforts.html?ref=business", 20],
+
+				["Unscramble", 
+					"The passage in the link below is the same as the article above with the paragraphs out of order. Put the paragraphs back in order. http://www.nytimes.com/2016/07/21/business/failure-of-2-health-insurer-mergers-is-unlikely-to-stop-the-efforts.html?ref=business/reorder", 20]
+			]
+		],
+
+	],
+
+	[
+		["Paragraph Headlines - Climate",
+			[
+				["Read Article",
+					"Read article at http://phys.org/news/2016-07-north-american-forests-climate.html", 20
+				],
+
+				["Write Headlines",
+					"Think about the main idea of each paragraph and write a 'headline' for each paragraphs. Each headline should be 6-8 words, that captures the main idea of the paragraph.", 25
+				],
+
+				["Summarize",
+					"Write 5-6 sentence summary of the entire article.", 20
+				]
+			]
+		]
+	],
+
+	[
+		["Reading Comprehension - Dinosaurs",
+			[
+				["Read Article",
+					"Read article from  ETS TOEFL Planner page 43 on Meteorite Impact and Dinosaur Extinction", 20
+				],
+
+				["Watch Video",
+					"Watch video at https://www.youtube.com/watch?v=61-ZvBm1cQo", 15
+				],
+		
+				["Quiz",
+					"Please answer the following question after reading the above article.",
+					{
+						Name: "Meteorite Impact and Dinosaur Extinction Quiz",
+						description: "Quiz on Meteorite Impact and Dinosaur Extinction",
+						passCutoff: 0.5,
+						time: 2,
+						questions: [
+							{
+							no: "1",
+							qtext: "The word “pose” on line 2 is closest in meaning to",
+							options: [{text: "Claim" }, {text: "Model"}, {text: "Assume"}, {text: "Present"}], 
+							ans: "Present",
+							marks: 1
+							},
+
+							{
+							no: "2",
+							qtext: "In paragraph 2, why does the author include the information that dinosaurs had flourished for tens of millions of years and then suddenly disappeared?",
+							options: [{text: "To support the claim that the mass extinction at the end of the Cretaceous is the best-documented of the dozen or so mass extinctions in the geological record" }, {text: "To explain why as many as half of the species on Earth at the time are believed to have become extinct at the end of the Cretaceous"}, {text: "To explain why paleontologists have always been intrigued by the mass extinction at the end of the Cretaceous"}, {text: "To provide evidence that an impact can be large enough to disturb the environment of the entire planet and cause an ecological disaster"}], 
+							ans: "To explain why paleontologists have always been intrigued by the mass extinction at the end of the Cretaceous",
+							marks: 1
+							},
+
+							{
+							no: "3",
+							qtext: "Which of the following can be inferred from paragraph 3 about the location of the meteorite impact in Mexico?",
+							options: [{text: "The location of the impact site in Mexico was kept secret by geologists from 1980 to 1990." }, {text: "It was a well-known fact that the impact had occurred in the Yucatán region."}, {text: "	Geologists knew that there had been an impact before they knew where it had occurred."}, {text: "The Yucatán region was chosen by geologists as the most probable impact site because of its climate."}], 
+							ans: "Geologists knew that there had been an impact before they knew where it had occurred.",
+							marks: 1
+							},
+
+							{
+							no: "4",
+							qtext: "According to paragraph 3, how did scientists determine that a large meteorite had impacted Earth?",
+							options: [{text: "They discovered a large crater in the Yucatán region of Mexico." }, {text: "They found a unique layer of sediment worldwide."}, {text: "They were alerted by archaeologists who had been excavating in the Yucatán region."}, {text: "They located a meteorite with a mass of over a trillion tons."}], 
+							ans: "They found a unique layer of sediment worldwide.",
+							marks: 1
+							}
+						]
+					}	
+				]
+			]
+		]
+	]
+];
+
+module.exports = reading;
